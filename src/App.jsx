495d6a7f95a17aaaa613b2c950b2972058bd22af
Swaraj Upreti <swarajupreti@gmail.com>
2024-11-07 14:10:29 +0530

    added the model section for 3d look

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Video from "./components/Video"
+import Model from "./components/Model"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Hero/>
       <Highlights/>
       <Video/>
+      <Model/>
     </main>
   )
 }
